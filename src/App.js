b742ca0777b3d8e4b3b30f8f8b37165057207715
Dakshraj Sharma <sharmarajdaksh@gmail.com>
2020-03-29 23:38:42 +0530

    Basic functionality works

--- src/App.js
+++ src/App.js
@@ -1,54 +1,57 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
 import { ElementList } from './components/ElementList';
+import { Header } from './components/Header';
 
-const App = () => {
-	// const [id, setId] = useState(4);
-	// const idGenerators = () => {
-	// 	setId(id + 1);
-	// 	console.log(id);
-	// 	return id - 1;
-	// };
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-	const [elements, setElements] = useState([
-		{ key: 2, value: 25 },
-		{ key: 1, value: 72 },
-		{ key: 3, value: 44 },
-		{ key: 4, value: 99 },
-		{ key: 5, value: 12 },
-		{ key: 6, value: 87 }
-	]);
+class App extends React.Component {
+	state = {
+		elements: [
+			{ key: 2, value: 25 },
+			{ key: 1, value: 72 },
+			{ key: 3, value: 44 },
+			{ key: 4, value: 99 },
+			{ key: 5, value: 12 },
+			{ key: 6, value: 87 }
+		]
+	};
 
-	const bubbleSort = () => {
-		let elementsLength = elements.length;
+	bubbleSort = async () => {
+		let elementsLength = this.state.elements.length;
 		for (let i = 0; i < elementsLength; i++) {
 			for (let j = 0; j < elementsLength - 1; j++) {
-				if (elements[j].value > elements[j + 1].value) {
-					let updatedElements = [...elements];
+				if (
+					this.state.elements[j].value >
+					this.state.elements[j + 1].value
+				) {
+					let updatedElements = [...this.state.elements];
 					let x = updatedElements[j];
 					updatedElements[j] = updatedElements[j + 1];
 					updatedElements[j + 1] = x;
-					setElements(updatedElements);
+					this.setState({ elements: updatedElements });
+					await sleep(750);
 				}
 			}
 		}
 	};
 
-	useEffect(() => {
-		console.log('element');
-	}, [elements]);
-
-	let sort = bubbleSort;
-
-	return (
-		<div>
-			<header>HELLOOOOO</header>
-			<div className='main'>
-				<button onClick={sort}>Let the sorting begin</button>
-				<ElementList elements={elements}></ElementList>
+	render() {
+		return (
+			<div>
+				<Header />
+				<div className='main'>
+					<button
+						className='element-list btn'
+						onClick={this.bubbleSort}
+					>
+						Let the sorting begin
+					</button>
+					<ElementList elements={this.state.elements}></ElementList>
+				</div>
 			</div>
-		</div>
-	);
-};
+		);
+	}
+}
 
 export default App;
